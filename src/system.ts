886a261b6e0d25d40e96de008381ee2b77f11f6a
hgy667 <119588753+hgy667@users.noreply.github.com>
2023-04-03 00:17:50 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -14,7 +14,7 @@ export const defaultMessage = `Powered by OpenAI Vercel
 - 本网站基于ChatGPT和Github开源项目制作 
 - 仅供独众传媒内部使用 切勿外传 避免网站被封！！！
 - 本网站由何高阳开发与维护，未经允许不得用于商业用途
-- 在使用过程中遇到任何问题可以联系本人微信 [喔嚯](https://qm.qq.com/cgi-bin/qm/qr?k=9sBonSEvwvNEA-vLiJHZpL4sAD2DouCW&noverify=0&personal_qrcode_source=4)
+- 在使用过程中遇到任何问题可以联系本人[QQ](https://qm.qq.com/cgi-bin/qm/qr?k=9sBonSEvwvNEA-vLiJHZpL4sAD2DouCW&noverify=0&personal_qrcode_source=4)
 ==================================
 - [[Shift]] + [[Enter]] 文字换行。开头输入 [[/]] 使用提问辅助。`
 
