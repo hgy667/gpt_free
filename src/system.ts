f6ff94527345473940dd27779f33eb899a982a43
hgy667 <119588753+hgy667@users.noreply.github.com>
2023-04-12 19:45:50 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -3,7 +3,7 @@ import type { Model } from "./types"
 export const defaultSetting = {
   continuousDialogue: true,
   archiveSession: true,
-  openaiAPIKey: "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",
+  openaiAPIKey: "",
   openaiAPITemperature: 60,
   password: "",
   systemRule: "",
