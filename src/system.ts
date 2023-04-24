2e0eed88758e3b82d8f322825268b9f4193f4fde
hgy667 <119588753+hgy667@users.noreply.github.com>
2023-04-24 15:12:04 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -13,7 +13,7 @@ export const defaultSetting = {
 export const defaultMessage = `chatgpt 3.5  大众体验版
 - 玩转GPT-3.5免费体验一天！ 
 - 我们公司排到了OPENAI的api，现在有接口大家可以免魔法，不用注册帐号就可以进群试用gpt3.5AI 大模型了，为了回馈大家，现在免费对大家开放社群，大家可以进来试用。
-- 为了避免造成拥堵，免费体验只限一天，联系客服可拿到VIP权限：[客服微信](https://www.yuque.com/u32987515/sh30ux/tqhrznho3lx61nho?singleDoc# 《客服微信》)
+- 为了避免造成拥堵，免费体验只限一天，联系客服可拿到VIP权限：[客服微信](https://www.yuque.com/u32987515/sh30ux/tqhrznho3lx61nho?singleDoc)
 - 也可自助续费：[淘宝自助入口](https://m.tb.cn/h.Uu1iPB2?tk=fgLVdO6Mupe)
 - VIP客户无需魔法，无需帐号可顺畅享受Chatgpt大模型带来的效率和体验！
 ==================================
